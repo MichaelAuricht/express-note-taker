c9d1ab01956fa78a4fdecbd18a34a61e55ceec2c
Michael Auricht <michaelauricht@live.com.au>
2022-05-16 23:58:40 +1000

    updating routes, adding server.js and others

--- db/store.js
+++ db/store.js
@@ -1,25 +1,44 @@
-const fs =require("fs");
-const util =require('util')
+const util = require("util");
+const fs = require("fs");
+const uuid = require("uuid").v1;
 
-const readFileAsync = util.promisify(fs.writeFile)
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 class Store {
-    read(){
-        return readFileAsync('db.json','utf8')
+    read() {
+        return readFileAsync("db/db.json", "utf8")
     }
-    //getNotes() to get the note
-    getNotes(){
-        return this.read().then((notes) => {
-            let parsedNotes
-            try{
-                parsedNotes = [].concat(JSON.parse(notes))
-            } catch (err){
-                parsedNotes = []
-            }
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
 
-            return parsedNotes
-        })
+    addNote(note) {
+        const { title, text } = note
+
+        if (!title || !text) {
+            throw new Error("title and text cannot be blank")
+        }
+
+        const newNote = { title, text, id: uuid() }
+
+        return this.getNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => this.newNote)
+    }
+
+    getNotes() {
+        return this.read()
+            .then(notes => {
+                return JSON.parse(notes) || [];
+            })
+    }
+    removeNote(id) {
+        return this.getNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(keptNotes => this.write(keptNotes))
     }
 }
 
-module.exports=new Store()
+module.exports = new Store();
