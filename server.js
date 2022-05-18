8992538b32abad73a5123f616c6e4401b6ef5dc1
Michael Auricht <michaelauricht@live.com.au>
2022-05-18 21:01:37 +1000

    test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+//test
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
